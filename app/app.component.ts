3f5a241803f652a262554056b36debf0235d1da6
Karan Bir <karan@upeo.com>
2016-08-24 00:05:38 +0530

    component changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,9 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>First Angular app going to Azure</h1>'
+    template: '<h1>First Angular app going to Azure : {{ display }} </h1>'
 })
-export class AppComponent { }
+export class AppComponent { 
+	display = " Hello World! ";
+
+}
